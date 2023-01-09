ad455ad958b436fab637a779354e68dbdb526759
Alex Yevtushenko <a@qx.zone>
2023-01-09 18:21:10 +0200

    Fix LCD Marquee String control rendering issue

--- lib/generic-controls/src/LcdMarqueeString.h
+++ lib/generic-controls/src/LcdMarqueeString.h
@@ -9,7 +9,10 @@ class LcdMarqueeString
   public:
     LcdMarqueeString(uint8_t display_len, uint16_t speed_ms = 330) : display_len(display_len), speed_ms(speed_ms)
     {
-      sprintf(str_format, "%%%us", display_len);
+      auto n = sprintf(str_format, "%%%us", display_len);
+      str_format[n] = 0;
+
+      textRenderBuffer = (char *)malloc(sizeof(char) * (display_len + 1));
     }
 
     void setText(String text)
@@ -45,7 +48,9 @@ class LcdMarqueeString
         text_changed = false;
 
         if (text.length() <= display_len) {
-          out->printf("%s%*c", text, display_len - text.length(), ' ');       // left-side
+          auto n = snprintf(textRenderBuffer, display_len, "%s%*c", text.c_str(), display_len - text.length(), ' ');
+          writeTextRenderBuffer(out, n);
+
           return;
         }
 
@@ -61,12 +66,18 @@ class LcdMarqueeString
         String buf = text.substring(left, left + len);
         if (buf.length() < display_len)
         {
-          if (current_offset >= text.length()) out->printf(str_format, buf);  // right-side
-          else out->printf("%s%*c", buf, display_len - buf.length(), ' ');    // left-side
+          if (current_offset >= text.length()) {    // right-side
+            auto n = snprintf(textRenderBuffer, display_len, str_format, buf.c_str());
+            writeTextRenderBuffer(out, n);
+          }
+          else {                                    // left-side
+            auto n = snprintf(textRenderBuffer, display_len, "%s%*c", buf.c_str(), display_len - buf.length(), ' ');
+            writeTextRenderBuffer(out, n);
+          }
         }
         else
         {
-          out->print(buf);
+          out->write(buf.c_str(), display_len);
         }
       }
     }
@@ -76,9 +87,30 @@ class LcdMarqueeString
     uint16_t speed_ms;
     int current_offset = 0;
     long now, last_draw = 0;
-    char str_format[5];
+    char str_format[5] = { 0, 0, 0, 0, 0 };
     bool text_changed = false;
     String text = emptyString;
+    char *textRenderBuffer;
+
+    inline size_t writeTextRenderBuffer(Print* out, int n)
+    {
+      n = n < 0 ? 0 : n > display_len ? display_len : n;
+      if (n > 0) {
+        textRenderBuffer[n] = 0;
+#ifdef DEBUG
+        auto writtenBytes = out->write(textRenderBuffer, n);
+        if (writtenBytes != n) {
+          log_w("Potential DATA LOSS during writing to Printable sink! sent=%u, written=%u", n, writtenBytes);
+        }
+
+        return writtenBytes;
+#else
+        return out->write(textRenderBuffer, n);
+#endif
+      }
+
+      return 0;
+    }
 
     void setInitialOffset()
     {
